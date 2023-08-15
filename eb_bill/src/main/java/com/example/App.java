f46d555458cb0597f9be5d858e418e594fd1c300
Nithish <kumarnithish6112002@gmail.com>
2023-08-15 18:43:04 +0530

    main

--- eb_bill/src/main/java/com/example/App.java
+++ eb_bill/src/main/java/com/example/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int name = s.next();
+        int name = s.nextInt();
         System.out.println(name);
         s.close();
     }
