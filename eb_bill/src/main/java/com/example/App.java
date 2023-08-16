43aa50522cfe9e12fa6321b26d8d4097a9b0eb6e
Nithish <kumarnithish6112002@gmail.com>
2023-08-16 22:31:03 +0530

    added UI

--- eb_bill/src/main/java/com/example/App.java
+++ eb_bill/src/main/java/com/example/App.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("\nEB BILL CALCULATOR ");
+        System.out.println("\nEnter your units of current used : ");
+
         int units = s.nextInt();
 
-        System.out.println( Calculations.segregate(units) );
+        System.out.format("\nBill Amount : %.1f\n" , Calculations.segregate(units));
         
         s.close();
     }
